4fbc0381895864202e4738d73920de94e517ea0f
Kyle Barnhouse <skyline6965@gmail.com>
2019-11-08 14:52:35 -0600

    working on routes, controllers, and models

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3001;
-const routes = require("./routes");
+const routes = require("./routes/api");
 const mongoose = require("mongoose");
 
 // Define middleware here
