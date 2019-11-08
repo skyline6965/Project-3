4581eea2d9d07e87603c7ade1b3d1eb7f08b75ac
race-briggs <race.briggs@gmail.com>
2019-11-08 11:51:09 -0600

    fixed start problem

--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -1,9 +1,15 @@
 import React from "react";
+import Navbar from "../components/Nav";
+import FormContainer from "../components/ContactForm/FormContainer";
 
 function Home(){
+    return(
     <div>
+        <Navbar/>
+        <FormContainer/>
 
     </div>
+    )
 }
 
-export default Home
+export default Home;
