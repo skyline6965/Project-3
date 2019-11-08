6aa8a7ae47799afa872cf319efd248e882d5473d
GCout <Gccoutret@gmail.com>
2019-11-08 13:51:44 -0600

    Added page for calander

---
+++ client/src/pages/calendar.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Navbar from "../components/Nav"
+import CalendarContainer from "../components/Calendar/Body"
+
+function CalendarPage() {
+  return(
+    <div>
+      <Navbar />
+      <CalendarContainer />
+    </div>
+  )
+};
+
+export default CalendarPage;
