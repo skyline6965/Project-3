584ffd17dccfd0bdccb7293870938417fa26a6fe
GCout <Gccoutret@gmail.com>
2019-11-11 11:05:30 -0600

    calendar now working

--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -1,13 +1,14 @@
 import React from "react";
 import Navbar from "../components/Nav";
 import FormContainer from "../components/ContactForm/FormContainer";
+import CalendarContainer from "../components/Calendar/Body";
 
 function Home(){
     return(
     <div>
         <Navbar/>
         <FormContainer/>
-
+        <CalendarContainer/>
     </div>
     )
 }
