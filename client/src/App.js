4ade6f8decf19aafd2f1b4d84b6e872fe65e9430
race-briggs <race.briggs@gmail.com>
2019-11-12 13:22:13 -0600

    added router to App.js and links to Navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,32 +1,36 @@
 import React from 'react';
 import Home from "./pages/home";
+import Calendar from "./pages/calendar";
+import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
 import './App.css';
-
 
 class App extends React.Component{
   render() {
   return (
-    <Home/>
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route path="/about">
 
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
-  );
+          </Route>
+          <Route path="/calendar">
+            <Calendar />
+          </Route>
+          <Route path="/login">
+
+          </Route>
+          <Route path="/register">
+
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+    );
+  }
 }
-}
-
-
+      
+      
 export default App;
