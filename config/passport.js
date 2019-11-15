c86ff2d830e98f99d22ebf44b4725d6a7d3a5945
race-briggs <race.briggs@gmail.com>
2019-11-15 10:51:50 -0600

    passport is WORKING

--- config/passport.js
+++ config/passport.js
@@ -5,8 +5,7 @@ var isAuthenticated = require("./middleware/isAuthenticated");
 
 
 function getUser(username){
-  let user = await db.User.find({ username: username });
-  return user
+  return db.User.find({ username: username });
 }
 
 passport.use(new LocalStrategy(
@@ -15,20 +14,14 @@ passport.use(new LocalStrategy(
     passwordField: "password"
   },
   function(username, password, done) {
-    getUser(username)
-    console.log("\n===========");
-    console.log(user);
-    console.log("user: " + user[0]);
-    console.log("password: " + user[0].password);
-    console.log("plain password: " + password);
-    console.log(db.User.checkPassword);
-    console.log("\n===========")
-    if (!user) {
-      return done(null, false, { message: 'incorrect username' });
-    } else if (!user.checkPassword(password)) {
-      return done(null, false, { message: 'incorrect password' });
-    }
-    return done(null, user);
+    getUser(username).then(users => {
+      if(users.length === 0) {
+        return done(null, false, { message: 'incorrect username' });
+      } else if (!users[0].checkPassword(password)) {
+        return done(null, false, { message: 'incorrect password' });
+      }
+      return done(null, users[0]);
+      })
     })
     // .catch(err => {console.log(err)})
   )
